44e71ebd610953c5d00fc64448b8f3a422edcf2d
ChristianGinko <christianginko@gmail.com>
2025-01-28 02:01:46 -0300

    Programa

--- app/src/main/java/com/example/proyectopokdex/SplashScreen.kt
+++ app/src/main/java/com/example/proyectopokdex/SplashScreen.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.proyectopokdex.navigation.AppScreens
@@ -42,11 +46,16 @@ fun Splash(){
         ){
             Image(
                 painterResource(R.drawable.pok_dex),
-                contentDescription = null
+                contentDescription = null,
+                modifier = Modifier
+                    .size(300.dp)
             )
             Text(
                 "Por Christian Ginko",
-                fontSize = 10.sp
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .offset(y = -50.dp)
             )
         }
     }
