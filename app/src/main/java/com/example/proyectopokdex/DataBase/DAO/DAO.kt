373ac39f7ce2f01a154a8b7b4a4c7b9bfc1a2526
ChristianGinko <christianginko@gmail.com>
2025-03-11 17:02:50 -0300

    Retrofit

--- app/src/main/java/com/example/proyectopokdex/DataBase/DAO/DAO.kt
+++ app/src/main/java/com/example/proyectopokdex/DataBase/DAO/DAO.kt
@@ -2,14 +2,20 @@ package com.example.proyectopokdex.DataBase.DAO
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.example.proyectopokdex.DataBase.Entities.Pokemon
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PokemonDao {
-    @Query("SELECT * FROM pokemon")
-    fun getAll(): List<Pokemon>
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(pokemon: Pokemon)
 
-    @Insert
-    fun insertAll(vararg pokemon: Pokemon)
+    @Update
+    suspend fun update(pokemon: Pokemon)
+
+    @Query("SELECT * FROM pokemon")
+    fun getAllPokemon(): Flow<List<Pokemon>>
 }
