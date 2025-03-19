4e4efa787ebbbc02f3d8ed6fd3e59d532c6eb112
ChristianGinko <christianginko@gmail.com>
2025-03-19 15:38:22 -0300

    Retrofit

--- app/src/main/java/com/example/proyectopokdex/Database/Database.kt
+++ app/src/main/java/com/example/proyectopokdex/Database/Database.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.proyectopokdex.entities.MyPoke
 
-@Database(entities = [MyPoke::class], version = 1, exportSchema = false)
+@Database(entities = [MyPoke::class], version = 2, exportSchema = false)
 abstract class PokemonDatabase : RoomDatabase() {
     abstract fun pokemonDao(): PokemonDao
 
